eb9bc322152a26a6d884593c4c53772b1c8130da
Yann Bizeul <yann@tynsoe.org>
2019-04-13 17:10:26 +0200

    Added a way to disable one update or the other

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,26 @@
-#!/bin/sh -x
+#!/bin/bash -x
 
 printenv > /etc/environment
 
-[ -n "$CRONTAB_UPDATE" ] && sed -E -i "/index.php update/{s,\* \* \* \* \*,$CRONTAB_UPDATE,g; }" $CRONTAB_FILE
-[ -n "$CRONTAB_DISCOVER" ] && sed -E -i "/index.php discover/{ s,\* \* \* \* \*,$CRONTAB_DISCOVER,g; }" $CRONTAB_FILE
+if [ -n "${CRONTAB_UPDATE}" ]
+then
+    if [ "${CRONTAB_UPDATE}" == "disabled" ]
+    then
+        sed -E -i "/index.php update/s,^,#," ${CRONTAB_FILE}
+    else
+        sed -E -i "/index.php update/{s,\* \* \* \* \*,$CRONTAB_UPDATE,g; }" ${CRONTAB_FILE}
+    fi
+fi
+
+if [ -n "${CRONTAB_DISCOVER}" ]
+then
+    if [ "${CRONTAB_DISCOVER}" == "disabled" ]
+    then
+        sed -E -i "/index.php discover/s,^,#," ${CRONTAB_FILE}
+    else
+        sed -E -i "/index.php discover/{s,\* \* \* \* \*,$CRONTAB_DISCOVER,g; }" ${CRONTAB_FILE}
+    fi
+fi
 
 crontab ${CRONTAB_FILE}
 
